ef60c395cca5a20223b8ad8333cdbb14987ae1dd
Edgar Rosas <me@edgarrosas.dev>
2022-05-13 20:25:50 -0700

    session actions

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,37 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import {
+  Controller,
+  Get,
+  Post,
+  Session as GetSession,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Session } from 'express-session';
 
-@Controller()
+type UserSession = Session & Record<'user', any>;
+@Controller('app')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  @Get('me')
+  getMe(@GetSession() session: UserSession) {
+    if (!session.user) throw new UnauthorizedException('Not authenticated');
+    return session.user;
+  }
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Post('auth')
+  auth(@GetSession() session: UserSession) {
+    session.user = {
+      id: 1,
+      email: 'me@edgarrosas.dev',
+    };
+    return 'auth successful';
+  }
+
+  @Post('logout')
+  logout(@GetSession() session: UserSession) {
+    return new Promise((resolve, reject) => {
+      session.destroy((err) => {
+        if (err) reject(err);
+        resolve(undefined);
+      });
+    });
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
+import * as session from 'express-session';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(
+    session({
+      secret: 'test-key',
+      resave: false,
+      saveUninitialized: false,
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
