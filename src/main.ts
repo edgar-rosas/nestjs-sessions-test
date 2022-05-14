c3c8a8939242d7d6c1d7f4cad57986e9916825e5
Edgar Rosas <me@edgarrosas.dev>
2022-05-13 20:34:07 -0700

    docker compose, redis

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import * as session from 'express-session';
+import * as connectRedis from 'connect-redis';
+import IoRedis from 'ioredis';
 import { AppModule } from './app.module';
+
+const RedisStore = connectRedis(session);
+const redisClient = new IoRedis('redis://localhost:6379');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(
     session({
+      store: new RedisStore({ client: redisClient }),
       secret: 'test-key',
       resave: false,
       saveUninitialized: false,
+      cookie: {
+        httpOnly: true,
+        maxAge: 1000 * 60 * 15,
+      },
     }),
   );
   await app.listen(3000);
